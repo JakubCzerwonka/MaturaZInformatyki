157fa4101b7b9c82c92a204bfab7bf875c3af749
Jakub Czerwonka <108996373+JakubCzerwonka@users.noreply.github.com>
2022-07-18 14:49:35 +0200

    Update rozkladLiczbyNaCzynnikiPierwsze.py

--- zagadnienia/algorytmy/rozkladLiczbyNaCzynnikiPierwsze/rozkladLiczbyNaCzynnikiPierwsze.py
+++ zagadnienia/algorytmy/rozkladLiczbyNaCzynnikiPierwsze/rozkladLiczbyNaCzynnikiPierwsze.py
@@ -1,20 +1,19 @@
 def rozloz(n):
     
-    # Stworzenie kopi liczby n 
-    d = n
-
-    # Tworzenie pustej tablicy oraz zmiennej 'k', żeby ją potem wykorzystać w roli dzielnika
-    nums = []
-    k = 2
-    
     # Jeśli liczba jest mniejsza od 1 lub równa 0 zwróć komunikat o wprowadzenia poprawnej liczby
     if n == 0:
         return "!!!!Liczba nie może być zerem!!!!"
-    if n < 1:
+    elif n < 1:
         return "!!!!Wprowadź liczbę całkowitą naturalną!!!!"
-    if type(n) != int:
+    elif type(n) != int:
         return "!!!!Wprowadź liczbę całkowitą naturalną!!!!"
 
+    # Stworzenie kopi n
+    copyN = n
+
+    # Tworzenie pustej tablicy 'czynniki' oraz zmiennej pomocniczej 'k'
+    nums = []
+    k = 2
 
     # Czy liczba jest różna od 1
     while n >= k: 
@@ -28,7 +27,7 @@ def rozloz(n):
 
     # Jeśli liczba ma tylko dwa dzielniki (1 i samą siebie), to jest liczbą pierwszą. W przypadku liczby 1 ma ona tylko jeden dzielnik.
     if len(nums) <= 1:
-        return "Liczba: ", d, "jest liczbą pierwszą"
+        return "Liczba: ", copyN, "jest liczbą pierwszą"
     
 
     return nums
