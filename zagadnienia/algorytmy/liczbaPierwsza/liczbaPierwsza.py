a34e80f53f3c37cd3ff895a52454ea4aeed0fb47
Jakub Czerwonka <108996373+JakubCzerwonka@users.noreply.github.com>
2022-07-18 13:38:42 +0200

    Update liczbaPierwsza.py

--- zagadnienia/algorytmy/liczbaPierwsza/liczbaPierwsza.py
+++ zagadnienia/algorytmy/liczbaPierwsza/liczbaPierwsza.py
@@ -8,6 +8,8 @@ def pierwsza(n):
         return "!!!!Liczba nie może być zerem!!!!"
     if n < 1:
         return "!!!!Wprowadź liczbę całkowitą naturalną!!!!"
+    if n != type(int):
+        return "!!!!Wprowadź liczbę całkowitą naturalną!!!!"
     
 
     # Utworzenie tablicy do której będą dodawane dzielniki liczby n
