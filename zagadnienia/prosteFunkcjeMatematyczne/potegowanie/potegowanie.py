505f0142d798d599f4e7e8284c4acb1a52bf78b5
Jakub Czerwonka <108996373+JakubCzerwonka@users.noreply.github.com>
2022-07-23 12:44:59 +0200

    Update potegowanie.py

--- zagadnienia/prosteFunkcjeMatematyczne/potegowanie/potegowanie.py
+++ zagadnienia/prosteFunkcjeMatematyczne/potegowanie/potegowanie.py
@@ -1,1 +1,15 @@
-1
+# Funkcje definiowane przez użytkownika
+def potegowanieDoDrugiej(n):
+
+    return n * n
+
+def potegowanieDoTrzeciej(n):
+
+    return n ** 3
+
+
+print(potegowanieDoDrugiej(3))
+print(potegowanieDoTrzeciej(3))
+
+# Funckja wbudowana. Pierwszy argument to liczba którą chcemy podnieść do potęgi, a drugi argument to do ilu chcemy podnieść do potęgi liczbe z pierwszego argumentu. 
+print(pow(2,2))
