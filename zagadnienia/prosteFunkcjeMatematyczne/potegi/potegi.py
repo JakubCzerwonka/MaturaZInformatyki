92ecc56f7b1fe8734ae7d3e391202489090403f7
Jakub Czerwonka <108996373+JakubCzerwonka@users.noreply.github.com>
2022-07-23 12:46:08 +0200

    Create potegi.py

---
+++ zagadnienia/prosteFunkcjeMatematyczne/potegi/potegi.py
@@ -0,0 +1,15 @@
+# Funkcje definiowane przez użytkownika
+def potegowanieDoDrugiej(n):
+
+    return n * n
+
+def potegowanieDoTrzeciej(n):
+
+    return n ** 3
+
+
+print(potegowanieDoDrugiej(3))
+print(potegowanieDoTrzeciej(3))
+
+# Funckja wbudowana. Pierwszy argument to liczba którą chcemy podnieść do potęgi, a drugi argument to do ilu chcemy podnieść do potęgi liczbe z pierwszego argumentu. 
+print(pow(2,2))
