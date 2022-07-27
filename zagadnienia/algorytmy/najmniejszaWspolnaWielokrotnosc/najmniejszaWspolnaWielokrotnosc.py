090fe8bf622605c712861f471f563fc1692743be
Jakub Czerwonka <108996373+JakubCzerwonka@users.noreply.github.com>
2022-07-27 10:09:11 +0200

    Create najmniejszaWspolnaWielokrotnosc.py

---
+++ zagadnienia/algorytmy/najmniejszaWspolnaWielokrotnosc/najmniejszaWspolnaWielokrotnosc.py
@@ -0,0 +1,25 @@
+# Funkcja zwracająca największy wspólny dzielnik liczb x i y
+def nwd(x, y):
+
+    while y != 0:
+
+        c = x % y
+        x = y
+        y = c
+
+    return x
+
+# Funkcja zwracająca najmniejszą wspólną wielokrotność liczb n i k
+def nww(n, k):
+
+    nwwR = nwd(n, k)
+
+    wynik = n * k // nwwR
+
+    return wynik
+
+
+n = int(input("Podaj liczbe n: "))
+k = int(input("Podaj liczbe k: "))
+
+print(nww(n, k))
