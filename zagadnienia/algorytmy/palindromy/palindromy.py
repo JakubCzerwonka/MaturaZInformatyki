a4e19aeac4d8a3a1434cdd4b34dfc403dd30eabd
Jakub Czerwonka <108996373+JakubCzerwonka@users.noreply.github.com>
2022-08-02 13:42:10 +0200

    Create palindromy.py

---
+++ zagadnienia/algorytmy/palindromy/palindromy.py
@@ -0,0 +1,12 @@
+def palindrom(n):
+    
+    nMale = n.replace(' ', '').lower()
+
+    if nMale == nMale[::-1]:
+        return n,'Jest Palindromem' 
+    else:
+        return n,'Nie jest Palindromem'
+
+
+x = input("Wpisz slowo: ")
+print(palindrom(x))
